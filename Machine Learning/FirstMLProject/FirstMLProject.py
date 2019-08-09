9fe2815308fb11eba2a8feaa860c5b42bdf63e6f
Talon Jones <taloncjones@gmail.com>
2019-08-09 11:10:15 -0700

    Add imports for machine learning

--- Machine Learning/FirstMLProject/FirstMLProject.py
+++ Machine Learning/FirstMLProject/FirstMLProject.py
@@ -3,3 +3,25 @@
 # Author: Talon Jones
 
 # Purpose: Begin learning ML
+
+import numpy as np
+import pandas as pd
+
+# Splits data into groups
+from sklearn.model_selection import train_test_split
+
+# Preprocessing module
+from sklearn import preprocessing
+
+# Model family
+from sklearn.ensemble import RandomForestRegressor
+
+# Cross-validation
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+
+# Metrics for model performance
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Persist model for the future
+from sklearn.externals import joblib
