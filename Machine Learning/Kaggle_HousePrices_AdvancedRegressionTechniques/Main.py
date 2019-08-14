367d004d96e588e89a14c92cbc588aa5709aff55
Talon Jones <taloncjones@gmail.com>
2019-08-14 12:21:46 -0700

    Add imports

---
+++ Machine Learning/Kaggle_HousePrices_AdvancedRegressionTechniques/Main.py
@@ -0,0 +1,7 @@
+# File: Machine Learning/Kaggle_HousePrices_AdvancedRegressionTechniques/Main.py
+# Use: The goal of this project is to begin learning ML and is taken from a tutorial found in the README
+# Author: Talon Jones
+
+# Import statements
+import numpy as np
+import pandas as pd
